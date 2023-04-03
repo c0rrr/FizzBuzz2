64ea73f3f0b4da06d33c19cbb3c61eb3c9219bd6
c0rrr <eryk.surel@onet.pl>
2023-04-03 21:09:11 +0200

    Dodaj pliki projektów.

---
+++ FizzBuzzWeb/Forms/FizzBuzzForm.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+
+namespace FizzBuzzWeb.Forms
+{
+    public class FizzBuzzForm
+    {
+        [Display(Name = "Twój szczęśliwy numerek")]
+        [Required, Range(1, 1000, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}.")]
+        public int? Number { get; set; }
+    }
+}
---
+++ FizzBuzzWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using FizzBuzzWeb.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics.Eventing.Reader;
+
+namespace FizzBuzzWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty(SupportsGet = true)]
+        public FizzBuzzForm FizzBuzz { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["AlertMessage"] = "Fizz";
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        }
+    }
+}
